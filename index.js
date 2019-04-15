72fb35c9f0910590ad2dce8109ace9521a2332a0
devCloud1_liuhengtao <liuhengtao@chinasofti.com>
2019-04-15 11:42:46 +0800

    5

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import MyBtn from './src/plugin/btn/btn';
 
-export function install(Vue) {
+const install = function (Vue) {
   Vue.component(MyBtn.name, MyBtn)
 }
+
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+const version = '1.0.5';
 export {
+  install,
+  version,
   MyBtn
+}
+export default {
+  install,
+  version
 }
